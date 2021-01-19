9d37d82959a00b0f4bb2d6a6245d2340544faab1
awais2075 <awais2075@gmail.com>
2021-01-19 12:25:29 +0500

    added routes for three pages [login_page:'/', survey_page:'survey_page', surveys_page:'surveys_page'

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_survey_app/pages/login_page.dart';
 import 'package:flutter_survey_app/pages/survey_page.dart';
+import 'package:flutter_survey_app/pages/surveys_page.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -11,30 +11,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: (RouteSettings settings) {
+        print('build route for ${settings.name}');
+        var routes = <String, WidgetBuilder>{
+          '/': (context) => LoginPage(),
+          '/survey_page': (context) => SurveyPage(),
+          '/surveys_page': (context) => SurveysPage(),
+        };
+        WidgetBuilder builder = routes[settings.name];
+        return MaterialPageRoute(builder: (ctx) => builder(ctx));
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: SurveyPage(),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: LoginPage(),
     );
   }
 }
-
-
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -64,9 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Image Picker Example'),
       ),
       body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
